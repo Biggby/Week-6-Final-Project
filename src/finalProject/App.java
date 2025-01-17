a9eb3a0986699ab3322e0ed9cf33b06ef74465af
Water <81544555+Biggby@users.noreply.github.com>
2025-01-17 14:44:19 -0500

    Game Update
    - Game Works - Has some issues with the Suits not always being described for what they are
    
    - Plays 26 rounds and announces winner
    
    - Card values are all good in the ArrayList's

--- src/finalProject/App.java
+++ src/finalProject/App.java
@@ -1,5 +1,69 @@
 package finalProject;
 
 public class App {
+    public static void main(String[] args) throws InterruptedException {
+        // Initialize deck and players
+        Deck deck = new Deck();
+        Player player1 = new Player("Player 1");
+        Player player2 = new Player("Player 2");
+        
+        // Shuffle deck
+        deck.shuffle();
+        
+        // Deal cards
+        System.out.println("Dealing cards...");
+        for (int i = 0; i < 26; i++) {
+            player1.draw(deck);
+            player2.draw(deck);
+        }
 
+        
+        // Game starts here
+        System.out.println("Game Start!\n");
+        
+        for (int i = 0; i < 26; i++) {
+            System.out.println("Round " + (i + 1) + ":");
+            
+            Card card1 = player1.flip();
+            Card card2 = player2.flip();
+            
+            // Players cards lay down
+            System.out.print("Player 1: ");
+            card1.describe();
+            Thread.sleep(500);
+            System.out.print("Player 2: ");
+            card2.describe();
+            
+            // Compare cards and award points
+            if (card1.getValue() > card2.getValue()) {
+                player1.incrementScore();
+                System.out.println("Player 1 wins this round!");
+            } else if (card2.getValue() > card1.getValue()) {
+                player2.incrementScore();
+                System.out.println("Player 2 wins this round!");
+            } else {
+                System.out.println("It's a tie! No points awarded.");
+            }
+            
+            // Print score
+            System.out.println("\nCurrent scores:");
+            System.out.println("Player 1: " + player1.getScore());
+            System.out.println("Player 2: " + player2.getScore());
+            System.out.println("------------------------");
+        }
+        
+        // Compare final scores and announce winner
+        System.out.println("\nGame Over!");
+        System.out.println("Final Scores:");
+        System.out.println("Player 1: " + player1.getScore());
+        System.out.println("Player 2: " + player2.getScore());
+        
+        if (player1.getScore() > player2.getScore()) {
+            System.out.println("Player 1 wins!");
+        } else if (player2.getScore() > player1.getScore()) {
+            System.out.println("Player 2 wins!");
+        } else {
+            System.out.println("Draw!");
+        }
+    }
 }
--- src/finalProject/Card.java
+++ src/finalProject/Card.java
@@ -1,5 +1,9 @@
 package finalProject;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Card {
 
 	private int value;
@@ -31,3 +35,122 @@ public class Card {
 	}
 	
 }
+
+class Deck {
+	private List<Card> cards;
+	
+	public Deck() {
+		cards = new ArrayList<>();
+		
+		String[] suits = {"Spades", "Hearts", "Clubs", "Diamond"};
+		String[] faces = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
+
+		
+		for (String suit : suits) {
+            for (int i = 0; i < faces.length; i++) {
+                // Set proper value (Ace = 14, Jack = 11, Queen = 12, King = 13)
+                int value;
+                if (faces[i].equals("A")) {
+                    value = 14;
+                } else if (faces[i].equals("J")) {
+                    value = 11;
+                } else if (faces[i].equals("Q")) {
+                    value = 12;
+                } else if (faces[i].equals("K")) {
+                    value = 13;
+                } else {
+                    value = Integer.parseInt(faces[i]);
+                }
+                
+                cards.add(new Card(value, faces[i] + " of " + suit));
+            }
+        }
+		
+		for(int i = 0; i < faces.length; i++) {
+			int cardValue = faces[i].equals("A") ? 14 : (i + 1);
+            String cardName = faces[i] + " of " + suits;
+			cards.add(new Card(cardValue, cardName));
+		
+		}
+	}
+
+
+
+
+	public void shuffle() {
+	    Collections.shuffle(cards);
+	}
+	
+	public Card draw() {
+		if(cards.isEmpty()) {
+			return null;
+		}
+		return cards.remove(0);
+	}
+}
+
+class Player {
+	private int score; // start of the score which stays private
+	private String name; // player name
+	private List<Card> hand;
+	
+	public Player(String name) {
+		this.hand = new ArrayList<>();
+		this.name = name;
+		this.score = 0;
+		
+	}
+	
+	public void describe() {
+		System.out.println(name + "' Hand: ");
+		for(Card card : hand) {
+			card.describe();
+		}
+		System.out.println("Score: " + score);
+	}
+
+	public Card flip() {
+		if (hand.isEmpty()) {
+			return null;
+		}
+		return hand.remove(0);
+	}
+	
+	public void draw(Deck deck) {
+	    Card card = deck.draw();
+	    if (card != null) {
+	        hand.add(card);
+	    }
+	}
+	
+	public void incrementScore() {
+	    score++;
+	}
+	
+	public int getScore() {
+	    return score;
+	}
+	
+	public String getName() {
+	    return name;
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
